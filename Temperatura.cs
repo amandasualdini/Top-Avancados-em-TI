99af38266c0a9d52b4e560c10d349003a9bf4a32
amandasualdini <58228941+amandasualdini@users.noreply.github.com>
2021-03-15 19:29:25 -0300

    Update Temperatura.cs

--- Temperatura.cs
+++ Temperatura.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercicios
+namespace Atividades
 {
-    class Ex11
+    class Temperatura
     {
         static void Main(string[] args)
         {
