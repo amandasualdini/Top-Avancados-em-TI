3b35133830b555d52d93eaf0e83b71a8f284c688
amandasualdini <58228941+amandasualdini@users.noreply.github.com>
2021-03-15 15:33:59 -0300

    Exercício 4 - Revendedora

---
+++ porcentagemRevendedora.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto
+{
+    class porcentagemRevendedora
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o seu salário: ");
+            double salario =  double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o seu salário: ");
+            double salario =  double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o nº de carros vendidos: "); 
+            int qtdCarro = int. Parse(Console.ReadLine());
+
+            Console.Write("Informe o val or total das vendas: ");
+            double valorTotalVendas =  double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o val or da comissão por carro vendido: "); 
+            double valorComissaoFixa =  double.Parse(Console.ReadLine());
+
+            double comissaoFixaFinal = qtdCarro * valorComissao Fixa;
+            double comissaoPercentual = valorTotalVendas * 0.05 ;
+
+            double salarioFinal = comissaoFixaFinal + comissaoP ercentual + salario;
+            Console.WriteLine("O salario final é {0:C}", salarioFinal);
+
+
+        }
+    }
+}
